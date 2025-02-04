9629141cac55da90e4282e36b45e145053227ba3
thesayyn <thesayyn@gmail.com>
2025-02-04 13:59:09 -0800

    fix: remove workspace support

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -16,7 +16,7 @@ git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel 6
+## Using BZLMOD
 
 1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
 2. Add to your \`MODULE.bazel\` file:
@@ -27,17 +27,5 @@ bazel_dep(name = "rules_distroless", version = "${TAG:1}")
 
 ## Using WORKSPACE
 
-Paste this snippet into your `WORKSPACE.bazel` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "rules_distroless",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/GoogleContainerTools/rules_distroless/releases/download/${TAG}/${ARCHIVE}",
-)
+We do not support WORKSPACE. Use Bzlmod instead.
 EOF
-
-awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
-echo "\`\`\`" 
